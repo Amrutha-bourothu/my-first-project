ec42d91363e3a7d0de9e9f2d3361144fc9421f38
Amrutha-bourothu <myselfammu676@gmail.com>
2026-04-01 18:28:31 +0530

    calculator project

---
+++ calculator.py
@@ -0,0 +1,27 @@
+print("=== Simple Calculator ===")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Choose operation:")
+print("+  -  *  /")
+
+op = input("Enter operation: ")
+
+if op == '+':
+    print("Result:", num1 + num2)
+
+elif op == '-':
+    print("Result:", num1 - num2)
+
+elif op == '*':
+    print("Result:", num1 * num2)
+
+elif op == '/':
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero")
+
+else:
+    print("Invalid operation")
